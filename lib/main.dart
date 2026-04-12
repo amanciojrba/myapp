e24a88fee7bc7c1334a19a1c0ad5822f6782163b
amanciojrba <amanciojrba@gmail.com>
2026-04-12 02:58:48 +0000

    Semana 7

--- lib/main.dart
+++ lib/main.dart
@@ -56,16 +56,19 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+void _incrementCounter() {
+  setState(() {
+    _counter++;
+  });
+}
+
+void _decrementCounter() {
+  setState(() {
+    _counter--;
+  });
+}
+    
+ 
 
   @override
   Widget build(BuildContext context) {
@@ -108,7 +111,16 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            ),TextButton(
+  onPressed: _incrementCounter,
+  child: Text('Increment'),
+),
+
+TextButton(
+  onPressed: _decrementCounter,
+  child: Text('Decrement'),
+),
+
           ],
         ),
       ),
