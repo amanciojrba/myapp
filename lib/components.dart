9596890b2a6aa9caccc705eb2be3e1f26addf268
amanciojrba <amanciojrba@gmail.com>
2026-04-27 01:23:08 +0000

    Semana 8 - Estrutura inicial com Scaffold e Center

--- lib/components.dart
+++ lib/components.dart
@@ -7,8 +7,33 @@ class ComponentsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Componentes')),
-      body: const Center(
-        child: Text('Página de Componentes'),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.deepPurple),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          height: 200,
+          child: SingleChildScrollView(
+            child: Column(
+              children: List.generate(
+                20,
+                (index) => Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: Text(
+                    'Item ${index + 1}',
+                    style: const TextStyle(
+                      fontSize: 18,
+                      color: Colors.deepPurple,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
